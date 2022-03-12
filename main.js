76d2f523fc79ecdca7c4139483a9b8c65c7059c1
Luis Cordero <corderoluisfernando/@gmail.com>
2022-03-12 00:46:25 -0500

    crud con local storage

--- main.js
+++ main.js
@@ -1,103 +1,112 @@
   
- window.addEventListener("load", actualizarPagina);
+ //window.addEventListener("load", actualizarPagina);
 
- function actualizarPagina () {
+// function actualizarPagina () { }
      
+    //Evento boton
+
+
+    const formulario = document.getElementById('form');
+    const listaDeCuentas = document.getElementById('listaActividades');
     
+    let datosInput = [];
 
-const columnaUno = document.createElement('div');
-      columnaUno.classList.add('columnas');
-      columnaUno.innerHTML = `
-      <hr/>
-      <h1>Lista de Cuentas</h1>
-      <table>
-        <thead>
-            <th>Tipo</th>
-            <th>Cuenta</th>
-            <th>Contraseña</th>
-            <th>Fecha</th>
-        </thead>
-        <tbody id="tbody">
-        </tbody>
-    </table>
-    `
-
-const columnaDos = document.createElement('div');
-      columnaDos.classList.add('columnas');
-      columnaDos.innerHTML = `
-      <h1>Ingrese los datos</h1>
-      <span>Tipo</span>
-      <input placeholder="Tipo de cuenta" type="text">
-      <span>Cuenta</span>
-      <input placeholder="Correo" type="text">
-      <span>Contraseña</span>
-      <input placeholder="Contraseña" type="text">
-      <span>Fecha</span>
-      <input type="date">
-      <button id="btn">enviar</button>
-    `
-
-const caja = document.createElement('div');
-      caja.classList.add('caja');
-      caja.insertAdjacentElement('afterbegin', columnaUno);
-      caja.insertAdjacentElement('afterbegin', columnaDos);
-
-    //Mi body
-let cuerpo = document.querySelector('body');
-    cuerpo.insertAdjacentElement('afterbegin', caja)
-
-    //Boton form
-    const boton = document.getElementById('btn');
-          boton.classList.add('btn')
-    boton.addEventListener('click', imprimir);
-    console.log(caja);
-
-
-    function imprimir(cuerpo) {
-
-        let inputUno = document.querySelectorAll('div input');
-        let condicion = 1;
-        if (condicion = 0 ) {
-           alert("hola")
-        } else {
-            
-              //Obtenemos elemento Tbody
-              const tbody = document.getElementById('tbody');
-              //Obtenemos los input
-                
-              //Creamos dinamicamente los tr y td
-                const tds = document.createElement('tr');
-                tds.innerHTML =  `
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              `
-          tbody.insertAdjacentElement('beforebegin', tds);
-          
-          // Le asignamos el valor de los inputs a los elementos td
-          tdUno = tds.children[0];
-          tdUno.innerHTML = inputUno[0].value;
-  
-           tdDos = tds.children[1];
-           tdDos.innerHTML = inputUno[1].value;
-  
-           tdTres = tds.children[2];
-           tdTres.innerHTML = inputUno[2].value;
-  
-           tdCuatro = tds.children[3];
-           tdCuatro.innerHTML = inputUno[3].value;
-  
-                 //Igualar inputs a Cero
-          for (let i = 0; i < inputUno.length; i++) {
-              const inp = inputUno[i]; 
-              inp.value = "";
-          }
-  
+    const crearItem = (tipo,cuenta,pass,fecha) => {
+        let item = {
+            tipo: tipo,
+            cuenta: cuenta,
+            pass: pass,
+            fecha: fecha
         }
-       
-    
-    }
-    
-}
 
+        datosInput.push(item);
+        return item;
+    }
+
+    const guardarLS = () => {
+        
+        localStorage.setItem('listas', JSON.stringify(datosInput));
+        imprimirLS();
+    }
+
+    const imprimirLS = () => {
+        
+        listaDeCuentas.innerHTML = "";
+
+        datosInput = JSON.parse(localStorage.getItem('listas'));
+        
+        if (datosInput === null) {
+            datosInput = [];
+        } else {
+            datosInput.forEach(element => {
+                listaDeCuentas.innerHTML += `
+                <div class="list">
+                    <li>${element.tipo}</li>
+                    <li>${element.cuenta}</li>
+                    <li>${element.pass}</li>
+                    <li>${element.fecha}</li>
+                    <li>Borrar</li>
+                </div>
+                `
+            });
+        }
+    }
+
+    const borrarLS = (tipo,cuenta,pass,fecha) => {
+
+        let indexArreglo;
+
+        datosInput.forEach((element, index) => {
+
+            if (element.tipo === tipo && element.cuenta === cuenta && element.pass === pass && element.fecha === fecha) {
+                indexArreglo = index;
+                console.log(index)
+            } 
+        });
+        datosInput.splice(indexArreglo, 1)
+        guardarLS();
+    }
+
+
+        
+
+
+        const boton = document.getElementById('btn');
+          boton.addEventListener('click', () => {
+
+                let inputs = document.querySelectorAll('input');
+                
+                let tipo = inputs[0].value;
+                let cuenta = inputs[1].value;
+                let pass = inputs[2].value;
+                let fecha = inputs[3].value;
+
+                crearItem(tipo, cuenta, pass, fecha);
+                guardarLS();
+                 
+                inputs.forEach(element => {
+                    element.value = "";
+                });
+                    
+          } );
+
+          document.addEventListener('DOMContentLoaded', imprimirLS)
+
+          listaDeCuentas.addEventListener('click', (e) => {
+
+                let textoUno = e.path[1].children[0].innerHTML;
+                let textoDos = e.path[1].children[1].innerHTML;
+                let textoTres = e.path[1].children[2].innerHTML;
+                let textoCuatro = e.path[1].children[3].innerHTML;
+
+           if (e.target.innerHTML === "Borrar") {
+                //console.log("estas presionando borrar")
+                //console.log(e.path[1].children[2].innerHTML)
+                borrarLS(textoUno, textoDos, textoTres, textoCuatro);
+           }
+
+          });
+
+  
+  
+    
